e77de05910e539431e8701fb4e7d0a870a1b5c12
ceb <henloboii@protonmail.ch>
2021-03-02 06:09:27 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ git clone --depth=1 --single-branch https://github.com/dahliaOS/buildroot /drone
 cd /drone/src/build/ 
 
 # Build 
+export FORCE_UNSAFE_CONFIGURE=1 # Disables the root check cuz it's just annoying 
 make&&qemu-system-x86_64 --enable-kvm -m 4096 -cdrom output/images/rootfs.iso9660&&cp output/images/rootfs.iso9660 output/images/rootfs.iso
 
 # Upload 
